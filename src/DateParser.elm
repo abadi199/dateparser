21dc8babeafb395f412abbccc079f033e06fbb24
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-02-24 08:05:48 -0600

    Add skeleton code

---
+++ src/DateParser.elm
@@ -0,0 +1,19 @@
+module DateParser exposing (parse)
+
+{-|
+Custom String to Date parser. This library uses date format from https://github.com/mgold/elm-date-format/blob/1.2.0/README.md
+
+# Parsing
+@docs parse
+-}
+
+import Date exposing (Date)
+
+
+{-| Parses `String` to a `Date`, and since this parsing can fail, it returns a `Result` with a `String` error message.
+Example:
+    parse "%d/%m/%Y" "31/12/2017" == Date.fromString "2017-12-31"
+-}
+parse : String -> String -> Result String Date
+parse pattern text =
+    Debug.crash ""
